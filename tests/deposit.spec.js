b2781a4eb172575bc486279691cdd8285ef29eda
S1PPAW1T <focus43002@gmail.com>
2026-02-26 21:47:53 +0700

    add-deposit-test

---
+++ tests/deposit.spec.js
@@ -0,0 +1,34 @@
+const { test, expect } = require('@playwright/test');
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('https://atm-buddy-lite.lovable.app/');
+
+    await page.getByPlaceholder('ตัวอย่าง: 123456').fill('468102');
+    await page.getByPlaceholder('รหัส PIN 4 หลัก').fill('4681');
+    await page.click('button:has-text("เข้าสู่ระบบ")');
+    await page.getByText('ฝากเงินDeposit').click();
+});
+
+test('Deposit success', async ({ page }) => {
+    await page.getByRole('spinbutton').fill('50000');
+    await page.click('button:has-text("ฝากเงิน")')
+
+    await expect(
+        page
+        .getByRole('region', { name: 'Notifications (F8)' })
+        .getByRole('status')
+        .filter({ hasText: 'ฝากเงินสำเร็จ' })
+    ).toBeVisible();
+});
+
+test('Deposit fail', async ({ page }) => {
+    await page.getByRole('spinbutton').fill('100001');
+    await page.click('button:has-text("ฝากเงิน")')
+
+    await expect(
+        page
+        .getByRole('region', { name: 'Notifications (F8)' })
+        .getByRole('status')
+        .filter({ hasText: 'ฝากเงินสำเร็จ' })
+    ).not.toBeVisible();
+});
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,31 +1,42 @@
 const { test, expect } = require('@playwright/test');
 
 test('Login success', async ({ page }) => {
-  await page.goto('https://atm-buddy-lite.lovable.app/');
+    await page.goto('https://atm-buddy-lite.lovable.app/');
 
-  await page.getByPlaceholder('ตัวอย่าง: 123456').fill('468102');
-  await page.getByPlaceholder('รหัส PIN 4 หลัก').fill('4681');
-  await page.click('button:has-text("เข้าสู่ระบบ")');
+    await page.getByPlaceholder('ตัวอย่าง: 123456').fill('468102');
+    await page.getByPlaceholder('รหัส PIN 4 หลัก').fill('4681');
+    await page.click('button:has-text("เข้าสู่ระบบ")');
 
-   await expect(page.getByRole('button', { name: 'ออกจากระบบ' })).toBeVisible();
+    await expect(page.getByRole('button', { name: 'ออกจากระบบ' })).toBeVisible();
 });
 
 test('Login wrong password', async ({ page }) => {
-  await page.goto('https://atm-buddy-lite.lovable.app/');
+    await page.goto('https://atm-buddy-lite.lovable.app/');
 
-  await page.getByPlaceholder('ตัวอย่าง: 123456').fill('468102');
-  await page.getByPlaceholder('รหัส PIN 4 หลัก').fill('9999');
-  await page.click('button:has-text("เข้าสู่ระบบ")');
+    await page.getByPlaceholder('ตัวอย่าง: 123456').fill('468102');
+    await page.getByPlaceholder('รหัส PIN 4 หลัก').fill('9999');
+    await page.click('button:has-text("เข้าสู่ระบบ")');
 
-   await expect(page.getByRole('button', { name: 'ออกจากระบบ' })).not.toBeVisible();
+    await expect(page.getByRole('button', { name: 'ออกจากระบบ' })).not.toBeVisible();
 });
 
 test('Login wrong username', async ({ page }) => {
-  await page.goto('https://atm-buddy-lite.lovable.app/');
+    await page.goto('https://atm-buddy-lite.lovable.app/');
 
-await page.getByPlaceholder('ตัวอย่าง: 123456').fill('468111');
-  await page.getByPlaceholder('รหัส PIN 4 หลัก').fill('4681');
-  await page.click('button:has-text("เข้าสู่ระบบ")');
+    await page.getByPlaceholder('ตัวอย่าง: 123456').fill('468111');
+    await page.getByPlaceholder('รหัส PIN 4 หลัก').fill('4681');
+    await page.click('button:has-text("เข้าสู่ระบบ")');
 
-   await expect(page.getByRole('button', { name: 'ออกจากระบบ' })).not.toBeVisible();
+    await expect(page.getByRole('button', { name: 'ออกจากระบบ' })).not.toBeVisible();
+});
+
+test('Logout success', async ({ page }) => {
+    await page.goto('https://atm-buddy-lite.lovable.app/');
+
+    await page.getByPlaceholder('ตัวอย่าง: 123456').fill('468102');
+    await page.getByPlaceholder('รหัส PIN 4 หลัก').fill('4681');
+    await page.click('button:has-text("เข้าสู่ระบบ")');
+
+    await page.getByRole('button', { name: 'ออกจากระบบ' }).click();
+    await expect(page.getByRole('button', { name: 'เข้าสู่ระบบ' })).toBeVisible();
 });
